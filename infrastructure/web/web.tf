fe26069f6f5ec5685ff29b8ac470ea9975aaebdb
MALOUNGILA <cmaloungila@gmail.com>
2018-01-04 10:19:21 +0100

    change to 0.0.3

--- infrastructure/web/web.tf
+++ infrastructure/web/web.tf
@@ -25,7 +25,7 @@ resource "azurerm_subnet" "subnet" {
 
 module "web" {
   #source = "../../compute/web"
-  source = "github.com:CMaloun/terraform-modules.git//compute/web?ref=v0.0.2"
+  source = "github.com/cmaloun/terraform-modules.git//compute/web?ref=v0.0.3"
   resource_group_name = "${var.resource_group_name}"
   location = "${var.location}"
   prefix = "${var.prefix}"
